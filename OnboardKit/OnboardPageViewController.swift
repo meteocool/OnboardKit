aed086bfbffccdda83a17c8052eca2a708790ce2
Nina2244 <26925042+Nina2244@users.noreply.github.com>
2021-01-21 01:44:37 +0100

    smaller image

--- OnboardKit/OnboardPageViewController.swift
+++ OnboardKit/OnboardPageViewController.swift
@@ -166,6 +166,8 @@ internal final class OnboardPageViewController: UIViewController {
   private func configureImageView(_ imageName: String?) {
     if let imageName = imageName, let image = UIImage(named: imageName) {
       imageView.image = image
+        
+        print(UIScreen.main.bounds)
         NSLayoutConstraint.activate([
               imageView.heightAnchor.constraint(equalTo: pageStackView.heightAnchor, multiplier: 0.4)
         ])
